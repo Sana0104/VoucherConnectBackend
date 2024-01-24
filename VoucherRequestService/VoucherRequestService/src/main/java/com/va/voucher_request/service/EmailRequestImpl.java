03027d678e366b6b30c604052877d457164f5e77
Sana0104 <83777249+Sana0104@users.noreply.github.com>
2024-01-24 09:56:36 +0530

    Update EmailRequestImpl.java

--- VoucherRequestService/VoucherRequestService/src/main/java/com/va/voucher_request/service/EmailRequestImpl.java
+++ VoucherRequestService/VoucherRequestService/src/main/java/com/va/voucher_request/service/EmailRequestImpl.java
@@ -1,32 +1,47 @@
 package com.va.voucher_request.service;
-
+ 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
-
+ 
 @Service
 public class EmailRequestImpl {
-
+ 
 	@Autowired
 	JavaMailSender javaMailSender;
-
+ 
 	@Value("${username}")
 	String username;
-
+ 
 	public String sendEmail(String toMail, String subject, String body) {
-
+ 
 		SimpleMailMessage message = new SimpleMailMessage();
-
+ 
 		message.setFrom(username);
 		message.setTo(toMail);
 		message.setSubject(subject);
 		message.setText(body);
-
+ 
 		javaMailSender.send(message);
-
+ 
 		return "mail send successfully";
 	}
+	public String sendPendingEmail(String toMail,String cc, String subject, String body) {
+ 
+		SimpleMailMessage message = new SimpleMailMessage();
 
+ 
+		message.setFrom(username);
+		message.setTo(toMail);
+		message.setCc(cc);
+		message.setSubject(subject);
+		message.setText(body);
+ 
+		javaMailSender.send(message);
+ 
+		return "mail send successfully";
+	}
+ 
 }
